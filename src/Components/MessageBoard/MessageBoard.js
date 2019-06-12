c7e7f4a0581b73ff67d8fa7a33c9ad28d4f93bcf
Arié Selinger <arie.selinger@gmail.com>
2019-06-12 12:22:04 +0200

    Add: MessageBoard

---
+++ src/Components/MessageBoard/MessageBoard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MessageBoard.css";
+
+class MessageBoard extends React.Component {
+
+    render() {
+        return (
+            <div class="container">
+                <div class="hello">Hello world</div>
+            </div>
+        )
+    }
+
+}
+
+export default MessageBoard
