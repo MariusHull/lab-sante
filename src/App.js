de486ba4e2ab506aecbf801bb09308f396deabb8
MariusHull <marius.hullin@gmail.com>
2019-06-12 15:41:54 +0100

    meh.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import socketIOClient from "socket.io-client";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import Test from "./Test";
+import Test from "./Compos/Test";
 
 class App extends Component {
   constructor() {
@@ -17,31 +16,8 @@ class App extends Component {
     };
   }
 
-  componentDidMount = () => {
-    const socket = socketIOClient(this.state.endpoint);
-    setInterval(this.send(), 1000);
-    socket.on("change color", col => {
-      document.body.style.backgroundColor = col;
-    });
-  };
-
-  // sending sockets
-  send = () => {
-    const socket = socketIOClient(this.state.endpoint);
-    socket.emit("change color", this.state.color); // change 'red' to this.state.color
-  };
-
-  // adding the function
-  setColor = color => {
-    this.setState({ color });
-  };
-
   // render method that renders in code if the state is updated
   render() {
-    const socket = socketIOClient(this.state.endpoint);
-    socket.on("change color", col => {
-      document.body.style.backgroundColor = col;
-    });
     return (
       <Router>
         <div>
---
+++ src/Compos/Test.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+import "../App.css";
+
+export default class Test extends Component {
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "localhost:3001",
+
+      ///
+      color: "white",
+      message: "",
+      ///
+      socket: "",
+      messages: [
+        { body: "Heyyyyy" },
+        { body: "Heyyyyy2" },
+        { body: "Heyyyyy3" }
+      ]
+    };
+  }
+
+  componentWillMount = () => {
+    this.state.socket = socketIOClient(this.state.endpoint);
+  };
+
+  onChange = e => {
+    let { message } = this.state;
+    message = e.target.value;
+    this.setState({ message });
+  };
+
+  onSubmit = e => {
+    console.log(this.state.message);
+    this.setState({ message: "" });
+    this.state.socket.emit("Message IOA", this.state.message);
+  };
+
+  // adding the function
+  setColor = color => {
+    this.setState({ color });
+  };
+
+  // render method that renders in code if the state is updated
+  render() {
+    const { socket, messages, message } = this.state;
+    socket.on("New message", mess => {
+      console.log("Messages : ", mess);
+      messages.push(mess);
+      this.setState({ messages });
+    });
+    return (
+      <div style={{ textAlign: "center" }}>
+        {/* <button onClick={() => this.send()}>Change Color</button>
+
+        <button id="blue" onClick={() => this.setColor("blue")}>
+          Blue
+        </button>
+        <button id="red" onClick={() => this.setColor("red")}>
+          Red
+        </button> */}
+        {messages &&
+          messages.map((message, index) => {
+            return (
+              <div>
+                <hb />
+                {message.body}
+              </div>
+            );
+          })}
+        <h2>Votre message : </h2>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            aria-describedby="emailHelp"
+            value={message}
+            onChange={this.onChange}
+            placeholder="Name"
+          />
+          <button onClick={this.onSubmit}>Envoyer</button>
+        </div>
+      </div>
+    );
+  }
+}
