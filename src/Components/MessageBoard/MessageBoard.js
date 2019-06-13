5d5b435d767b55a3e8560b9cd03700633557eabf
Arié Selinger <arie.selinger@gmail.com>
2019-06-13 17:24:06 +0200

    Add: Font Monsterrat + messages state

--- src/Components/MessageBoard/MessageBoard.js
+++ src/Components/MessageBoard/MessageBoard.js
@@ -1,10 +1,59 @@
 import React from "react";
 import "./MessageBoard.css";
+import * as moment from 'moment';
+import 'moment/locale/fr';
 
 class MessageBoard extends React.Component {
 
+
+    
     constructor() {
         super()
+        moment.locale('fr');
+        this.state = {messageList : [
+            {
+                sender: "IOA",
+                receiver: "Bocal",
+                body: "Aide demandée au Box 3.",
+                updated_at: Date.now(),
+                status: "important"
+            }, 
+            {
+                sender: "Bocal",
+                receiver: "IOA",
+                body: "Ceci est un test.",
+                updated_at: Date.now(),
+                status: "urgent"
+            },
+            {
+                sender: "Bocal",
+                receiver: "IOA",
+                body: "Ceci est un test.",
+                updated_at: Date.now(),
+                status: "urgent"
+            },
+            {
+                sender: "Accueil",
+                receiver: "IOA",
+                body: "Malaise jeune fille 12 ans.",
+                updated_at: Date.now(),
+                status: "urgent"
+            },
+            {
+                sender: "Bocal",
+                receiver: "IOA",
+                body: "Box 3 libéré.",
+                updated_at: Date.now(),
+                status: "urgent"
+            },
+            {
+                sender: "Accueil",
+                receiver: "IOA",
+                body: "Attention : deux frères sont arrivés aux urgences. Ne confondez pas les dossiers.",
+                updated_at: Date.now(),
+                status: "urgent"
+            }
+        ]}
     }
 
     colorFromSender(sender) {
@@ -25,61 +74,23 @@ class MessageBoard extends React.Component {
             <div class="message" style={{ borderColor: this.colorFromSender(message.sender) }}>
                 <div class="sender-container" style={{ backgroundColor: this.colorFromSender(message.sender) }}>
                     <div class="sender-transparent">
-                        <div class="sender-content">{message.sender}</div>
+                        <div class="sender-content" style={{fontWeight: 900, fontSize: 18}}>{message.sender}</div>
+                        <div>{"à " + moment(message.updated_at).format('LT')}</div>
                     </div>
                 </div>
-                <div class="message-content">
-                    {message.body}
+                <div class="message-container">
+                    <div class="message-content">{message.body}</div>
                 </div>
             </div>
         )
     }
 
     render() {
+        console.log(this.state)
+
         return (
             <div class="main-container">
-                {this.displayMessage({
-                    sender: "IOA",
-                    receiver: "Bocal",
-                    body: "Aide demandée au Box 3.",
-                    updated_at: Date.now(),
-                    status: "important"
-                })}
-                {this.displayMessage({
-                    sender: "Bocal",
-                    receiver: "IOA",
-                    body: "Ceci est un test.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                })}
-                {this.displayMessage({
-                    sender: "Cadres Sup",
-                    receiver: "IOA",
-                    body: "Ceci est un test.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                })}
-                {this.displayMessage({
-                    sender: "Accueil",
-                    receiver: "IOA",
-                    body: "Malaise jeune fille 12 ans.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                })}
-                {this.displayMessage({
-                    sender: "Bocal",
-                    receiver: "IOA",
-                    body: "Box 3 libéré.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                })}
-                {this.displayMessage({
-                    sender: "Accueil",
-                    receiver: "IOA",
-                    body: "Attention : deux frères sont arrivés aux urgences. Ne confondez pas les dossiers.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                })}
+                {this.state.messageList.map(message=>{return this.displayMessage(message)})}
             </div>
         )
     }
