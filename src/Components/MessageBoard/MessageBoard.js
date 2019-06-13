1dad03a0ad96ad0306da848e14875fdf77a827e9
Arié Selinger <arie.selinger@gmail.com>
2019-06-13 15:45:46 +0200

    Add: messages for message board

--- src/Components/MessageBoard/MessageBoard.js
+++ src/Components/MessageBoard/MessageBoard.js
@@ -3,10 +3,69 @@ import "./MessageBoard.css";
 
 class MessageBoard extends React.Component {
 
+    constructor(){
+        super()
+    }
+
+    colorFromSender(sender) {
+        switch (sender) {
+            case "IOA":
+                return "blue"
+            case "Bocal":
+                return "green" 
+            case "Accueil":
+                    return "orange"
+            default:
+                return "red"
+        }
+    }
+
+    displayMessage(message) {
+        return (
+            <div class="message" style={{ borderColor: this.colorFromSender(message.sender) }}>
+                <div class="sender-container" style={{ backgroundColor: this.colorFromSender(message.sender) }}>
+                    <div class="sender-transparent">
+                        <div class="sender-content">{message.sender}</div>
+                    </div>
+                </div>
+                <div class="message-content">
+                    {message.body}
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div class="container">
-                <div class="hello">Hello world</div>
+                {this.displayMessage({
+                    sender: "IOA",
+                    receiver: "Bocal",
+                    body: "Aide demandée au Box 3.",
+                    updated_at: Date.now(),
+                    status: "important"
+                })}
+                {this.displayMessage({
+                    sender: "Bocal",
+                    receiver: "IOA",
+                    body: "Ceci est un test.",
+                    updated_at: Date.now(),
+                    status: "urgent"
+                })}
+                {this.displayMessage({
+                    sender: "Cadres Sup",
+                    receiver: "IOA",
+                    body: "Ceci est un test.",
+                    updated_at: Date.now(),
+                    status: "urgent"
+                })}
+                {this.displayMessage({
+                    sender: "Accueil",
+                    receiver: "IOA",
+                    body: "Ceci est un test.",
+                    updated_at: Date.now(),
+                    status: "urgent"
+                })}
             </div>
         )
     }
