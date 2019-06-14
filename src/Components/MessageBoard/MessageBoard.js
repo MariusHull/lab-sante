0fe3ba7b01b911dbc90bb337d47ce65c250e7f04
Arié Selinger <arie.selinger@gmail.com>
2019-06-14 11:56:35 +0200

    Update: sender-container

--- src/Components/MessageBoard/MessageBoard.js
+++ src/Components/MessageBoard/MessageBoard.js
@@ -5,55 +5,55 @@ import 'moment/locale/fr';
 
 class MessageBoard extends React.Component {
 
-
-    
     constructor() {
         super()
         moment.locale('fr');
-        this.state = {messageList : [
-            {
-                sender: "IOA",
-                receiver: "Bocal",
-                body: "Aide demandée au Box 3.",
-                updated_at: Date.now(),
-                status: "important"
-            }, 
-            {
-                sender: "Bocal",
-                receiver: "IOA",
-                body: "Ceci est un test.",
-                updated_at: Date.now(),
-                status: "urgent"
-            },
-            {
-                sender: "Bocal",
-                receiver: "IOA",
-                body: "Ceci est un test.",
-                updated_at: Date.now(),
-                status: "urgent"
-            },
-            {
-                sender: "Accueil",
-                receiver: "IOA",
-                body: "Malaise jeune fille 12 ans.",
-                updated_at: Date.now(),
-                status: "urgent"
-            },
-            {
-                sender: "Bocal",
-                receiver: "IOA",
-                body: "Box 3 libéré.",
-                updated_at: Date.now(),
-                status: "urgent"
-            },
-            {
-                sender: "Accueil",
-                receiver: "IOA",
-                body: "Attention : deux frères sont arrivés aux urgences. Ne confondez pas les dossiers.",
-                updated_at: Date.now(),
-                status: "urgent"
-            }
-        ]}
+        this.state = {
+            messageList: [
+                {
+                    sender: "IOA",
+                    receiver: "Bocal",
+                    body: "Aide demandée au Box 3.",
+                    updated_at: Date.now(),
+                    status: "important"
+                },
+                {
+                    sender: "Bocal",
+                    receiver: "IOA",
+                    body: "Ceci est un test.",
+                    updated_at: Date.now(),
+                    status: "urgent"
+                },
+                {
+                    sender: "Bocal",
+                    receiver: "IOA",
+                    body: "Ceci est un test.",
+                    updated_at: Date.now(),
+                    status: "urgent"
+                },
+                {
+                    sender: "Accueil",
+                    receiver: "IOA",
+                    body: "Malaise jeune fille 12 ans.",
+                    updated_at: Date.now(),
+                    status: "urgent"
+                },
+                {
+                    sender: "Bocal",
+                    receiver: "IOA",
+                    body: "Box 3 libéré.",
+                    updated_at: Date.now(),
+                    status: "urgent"
+                },
+                {
+                    sender: "Accueil",
+                    receiver: "IOA",
+                    body: "Attention : deux frères sont arrivés aux urgences. Ne confondez pas les dossiers.",
+                    updated_at: Date.now(),
+                    status: "urgent"
+                }
+            ]
+        }
     }
 
     colorFromSender(sender) {
@@ -74,7 +74,7 @@ class MessageBoard extends React.Component {
             <div class="message" style={{ borderColor: this.colorFromSender(message.sender) }}>
                 <div class="sender-container" style={{ backgroundColor: this.colorFromSender(message.sender) }}>
                     <div class="sender-transparent">
-                        <div class="sender-content" style={{fontWeight: 900, fontSize: 18}}>{message.sender}</div>
+                        <div class="sender-content" style={{ fontWeight: 900, fontSize: 18 }}>{message.sender}</div>
                         <div>{"à " + moment(message.updated_at).format('LT')}</div>
                     </div>
                 </div>
@@ -85,12 +85,29 @@ class MessageBoard extends React.Component {
         )
     }
 
+    displayEmergencyMessage(message){
+        return(
+            <div class="emergency-container">
+                <div class="emergency-popup">
+                    <div>{message.body}</div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         console.log(this.state)
 
         return (
             <div class="main-container">
-                {this.state.messageList.map(message=>{return this.displayMessage(message)})}
+                {this.state.messageList.map(message => { return this.displayMessage(message) })}
+                {this.displayEmergencyMessage({
+                    sender: "Accueil",
+                    receiver: "IOA",
+                    body: "Accident de bus. 33 personnes blessées.",
+                    updated_at: Date.now(),
+                    status: "urgent"
+                })}
             </div>
         )
     }
