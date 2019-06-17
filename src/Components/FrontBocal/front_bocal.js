529520300bfa2fd9fc7ed8f7514cad187a3d53a2
Pierre Ripailles <pierre.ripailles@student.ecp.fr>
2019-06-17 14:38:14 +0200

    css3

--- src/Components/FrontBocal/front_bocal.js
+++ src/Components/FrontBocal/front_bocal.js
@@ -118,6 +118,8 @@ export default class FrontBocal extends Component {
   };
 
   render() {
+    const { nombrepatienturgences } = this.state;
+
     return (
       <div>
         <Navbar />
@@ -138,8 +140,17 @@ export default class FrontBocal extends Component {
                   <div className="messagetextbocal">Important : {message.body}</div>
                 ))}
             </div>
-            <div className="boxalarmbocal"> </div>
-          </div>
+            <div
+              className={`boxalarmbocal ${
+                nombrepatienturgences && nombrepatienturgences > 60
+                  ? "red"
+                  : nombrepatienturgences < 30
+                  ? "green"
+                  : "orange"
+              }`}
+            >
+            </div>          
+            </div>
 
           <div className="row2bocal">
             <div className="row2col1bocal">
@@ -155,10 +166,10 @@ export default class FrontBocal extends Component {
                 </div>
                 <div className="row2col1row1col1rowbocal">
                   <div className="row2col1row1col1bocal">
-                    Nombre de patient en attente d'bocal:
+                    Nombre de patient en attente d'IAO:
                   </div>
                   <div className="row2col1row1col2bocal">
-                    {this.state.nombrepatientbocal}
+                    {this.state.nombrepatientIAO}
                   </div>
                 </div>
                 <div className="row2col1row1col1rowbocal">
