79302730f948544d1b7f7b77f13ff31383887523
Arié Selinger <arie.selinger@gmail.com>
2019-06-19 13:43:05 +0200

    Add: slider + resize window

--- src/Components/MessageBoard/MessageBoard.js
+++ src/Components/MessageBoard/MessageBoard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
 
 import "./MessageBoard.css";
 import * as moment from "moment";
@@ -8,60 +10,62 @@ import socketIOClient from "socket.io-client";
 import Navbar from "../../Containers/Navbar";
 const socket = socketIOClient("localhost:3001");
 
+
 class MessageBoard extends React.Component {
 
-    constructor() {
-        super()
-        moment.locale('fr');
-        this.state = {
-            emergency: null,
-            numberRows: 5,
-            messageList: [
-                {
-                    sender: "IOA",
-                    receiver: "Bocal",
-                    body: "Aide demandée au Box 3.",
-                    updated_at: Date.now(),
-                    status: "important"
-                },
-                {
-                    sender: "Bocal",
-                    receiver: "IOA",
-                    body: "Ceci est un test.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                },
-                {
-                    sender: "Bocal",
-                    receiver: "IOA",
-                    body: "Ceci est un test.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                },
-                {
-                    sender: "Accueil",
-                    receiver: "IOA",
-                    body: "Malaise jeune fille 12 ans.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                },
-                {
-                    sender: "Bocal",
-                    receiver: "IOA",
-                    body: "Box 3 libéré.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                },
-                {
-                    sender: "Accueil",
-                    receiver: "IOA",
-                    body: "Attention : deux frères sont arrivés aux urgences. Ne confondez pas les dossiers.",
-                    updated_at: Date.now(),
-                    status: "urgent"
-                }
-            ]
+  constructor() {
+    super()
+    moment.locale('fr');
+    this.state = {
+      emergency: null,
+      oldMessageIndex: 0,
+      numberRows: 6,
+      messageList: [
+        {
+          sender: "IOA",
+          receiver: "Bocal",
+          body: "Aide demandée au Box 3.",
+          updated_at: Date.now(),
+          status: "important"
+        },
+        {
+          sender: "Bocal",
+          receiver: "IOA",
+          body: "Ceci est un test.",
+          updated_at: Date.now(),
+          status: "urgent"
+        },
+        {
+          sender: "Bocal",
+          receiver: "IOA",
+          body: "Ceci est un test.",
+          updated_at: Date.now(),
+          status: "urgent"
+        },
+        {
+          sender: "Accueil",
+          receiver: "IOA",
+          body: "Malaise jeune fille 12 ans.",
+          updated_at: Date.now(),
+          status: "urgent"
+        },
+        {
+          sender: "Bocal",
+          receiver: "IOA",
+          body: "Box 3 libéré.",
+          updated_at: Date.now(),
+          status: "urgent"
+        },
+        {
+          sender: "Accueil",
+          receiver: "IOA",
+          body: "Attention : deux frères sont arrivés aux urgences. Ne confondez pas les dossiers.",
+          updated_at: Date.now(),
+          status: "urgent"
         }
-    };
+      ]
+    }
+  };
 
 
   colorFromSender(sender) {
@@ -78,6 +82,13 @@ class MessageBoard extends React.Component {
         return "pink";
     }
   }
+
+  resizeWindow(){
+    let numberRows = Math.trunc((window.innerHeight - 165) / 75);
+    this.setState({numberRows : numberRows});
+    // console.log("numberRows", numberRows);
+  }
+
 
   displayMessage(message, index) {
     return (
@@ -117,15 +128,28 @@ class MessageBoard extends React.Component {
     );
   }
 
-    displayOldMessages(messages) {
-        return (
-            <div className="old-messages-container">
-                <div class="old-message-title">Anciens messages</div>
-                <div class="old-message-count">1/5</div>
-                <div class="old-message">{this.displayMessage(this.state.messageList[this.state.messageList.length-1], 0)}</div>
-            </div>
-        )
-    }
+  displayOldMessages(messages) {
+    return (
+      <div className="old-messages-container">
+        <div class="old-message-title">Anciens messages</div>
+        <div class="old-message-count">{this.state.oldMessageIndex + 1}/{messages.length}</div>
+        <Slider
+          autoplay={10000}
+          infinite={true}
+          previousButton={null}
+          nextButton={null}
+          onSlideChange={(event) => { this.setState({ oldMessageIndex: event.slideIndex }) }}>
+          {
+            messages.map((message, index) => {
+              return (
+                <div class="old-message">{this.displayMessage(message, index)}</div>
+              )
+            })
+          }
+        </Slider>
+      </div>
+    )
+  }
 
   componentWillMount = () => {
     // Getting all messages
@@ -145,11 +169,27 @@ class MessageBoard extends React.Component {
         this.setState({ emergency: message }, () => {
           setTimeout(() => {
             this.setState({ emergency: null });
-          }, 2000);
+          }, 5000);
         });
       }
     });
   };
+
+
+  /**
+   * Add event listener
+   */
+  componentDidMount() {
+    this.resizeWindow();
+    window.addEventListener("resize", this.resizeWindow.bind(this));
+  }
+
+  /**
+   * Remove event listener
+   */
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.resizeWindow.bind(this));
+  }
 
   render() {
     return (
@@ -165,7 +205,9 @@ class MessageBoard extends React.Component {
             {this.state.emergency &&
               this.displayEmergencyMessage(this.state.emergency)}
           </div>
-          {this.displayOldMessages()}
+          <div class="old-message-container-position">
+            {this.state.messageList.slice(this.state.numberRows).length > 0 && this.displayOldMessages(this.state.messageList.slice(this.state.numberRows))}
+          </div>
         </div>
       </div>
     );
